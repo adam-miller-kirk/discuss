2d090f7309f3bfb6332c6d53df59bf9cb2f1af64
Adam Miller-Kirk <adam_miller-kirk@hotmail.com>
2025-05-23 12:58:03 +0100

    feature enabled sign on and used basic messaging to check system works for server and ui systems

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,4 @@
-"use server";
-
-import * as auth from "@/auth";
-
-export async function signIn() {
-  return auth.signIn("github");
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn, signOut } from "./auth";
+export { createComment } from "./create-comment";
+export { createTopic } from "./create-topic";
+export { createPost } from "./create-post";
---
+++ src/app/topics/[slug]/posts/new/page.tsx
@@ -0,0 +1,3 @@
+export default function PostNewPage() {
+  return <div>Post New</div>;
+}
